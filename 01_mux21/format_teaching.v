f4faabde8be8adc87b1800798547f0d21ee0655c
wangchenxuhit <60310779+wangchenxuhit@users.noreply.github.com>
2020-03-19 06:20:58 +0800

    Formatting initial block.
    For teaching.

--- 01_mux21/format_teaching.v
+++ 01_mux21/format_teaching.v
@@ -15,7 +15,7 @@ module cnt16_tb ();
   always #10 tb_clk = ~tb_clk;
 
   initial begin
-    tb_rst_n = 1'b1;
+        tb_rst_n = 1'b1;
     #11 tb_rst_n = 1'b0;
     #25 tb_rst_n = 1'b1;
     #100 $finish;
@@ -30,13 +30,13 @@ reg              tb_clk, tb_rst_n;
 cnt16 dut 
 (.clk(tb_clk), .rst_n(tb_rst_n), cnt(tb_cnt));
 initial begin
-      tb_clk = 1¡¯b0;
+      tb_clk = 1Â¡Â¯b0;
 end
 always #10 tb_clk = ~tb_clk;
 initial begin
-      tb_rst_n = 1¡¯b1;
-      #11 tb_rst_n = 1¡¯b0;
-      #25 tb_rst_n = 1¡¯b1;
+      tb_rst_n = 1Â¡Â¯b1;
+      #11 tb_rst_n = 1Â¡Â¯b0;
+      #25 tb_rst_n = 1Â¡Â¯b1;
       #100 $finish;
 end
 endmodule
